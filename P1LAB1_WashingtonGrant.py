865955f7663c50732be8640f2bb1e7fbce8296bc
GrantW23 <washingg1002@student.faytechcc.edu>
2024-09-22 20:09:27 -0400

    First Submission

---
+++ P1LAB1_WashingtonGrant.py
@@ -0,0 +1,8 @@
+#Grant
+#9/22/24
+#P1lab
+#Allows user to input information and prints a response
+
+FirstName=input('Enter your first name: ')
+LastName= input('Enter your last name: ')
+print('Hello,',FirstName ,LastName ,'! Welcome to CTI-110')
