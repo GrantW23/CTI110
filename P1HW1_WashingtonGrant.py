84963c6586be8e1ddb4dcb472c4e946a04167099
GrantW23 <washingg1002@student.faytechcc.edu>
2024-09-29 19:27:17 -0400

    P1HW1_WashingtonGrant
    second Upload

---
+++ P1HW1_WashingtonGrant.py
@@ -0,0 +1,24 @@
+#Grant Washington
+#09/29/24
+#P1HW1
+#tests student's knowledge of how to write code that collects information from user, processes information collected and display results to user.
+
+
+print("----Calculating Exponents----")
+print(' ')
+first_integer= int(input("Enter an integer as the base value: " ))
+second_integer= int(input("Enter an integer as the exponent:" ))
+calculated_number= first_integer ** second_integer
+print(' ')
+print(first_integer, "rasied to the", second_integer,"is",calculated_number)
+print(' ')
+print("----Addition and Subtraction----")
+print(' ')
+start_int= int(input("Enter a starting integer:" ))
+add_int= int(input("Enter a integer to add:" ))
+sub_int= int(input("Enter a integer to add:" ))
+total_int=start_int+add_int-sub_int
+print(' ')
+print(start_int,"+",add_int,"-",sub_int,"is equal to", total_int)
+
+
